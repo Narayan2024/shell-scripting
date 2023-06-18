a0b4d0dd7e1c892e222cb3d3d94813539ae62029
Lakshmi <narayan.training1@gmail.com>
2023-06-18 17:59:44 +0530

    auto push

--- roboshop/Components/frontend.sh
+++ roboshop/Components/frontend.sh
@@ -1,3 +1,66 @@
-#! /bin/bash
+#!/bin/bash
 
-echo "I am frontend"
+echo -e "Installing nginx"
+yum install nginx -y 
+
+# The frontend is the service in RobotShop to serve the web content over Nginx.
+
+# Install Nginx.
+
+# ```
+# # yum install nginx -y
+# # systemctl enable nginx
+# # systemctl start nginx
+
+# ```
+
+# Let's download the HTDOCS content and deploy it under the Nginx path.
+
+# ```
+# # curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+
+# ```
+
+# Deploy in Nginx Default Location.
+# #! /bin/bash
+
+# The frontend is the service in RobotShop to serve the web content over Nginx.
+
+# Install Nginx.
+
+# ```
+# # yum install nginx -y
+# # systemctl enable nginx
+# # systemctl start nginx
+
+# ```
+
+# Let's download the HTDOCS content and deploy it under the Nginx path.
+
+# ```
+# # curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+
+# ```
+
+# Deploy in Nginx Default Location.
+# #! /bin/bash
+
+# The frontend is the service in RobotShop to serve the web content over Nginx.
+
+# Install Nginx.
+
+# ```
+# # yum install nginx -y
+# # systemctl enable nginx
+# # systemctl start nginx
+
+# ```
+
+# Let's download the HTDOCS content and deploy it under the Nginx path.
+
+# ```
+# # curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+
+# ```
+
+# Deploy in Nginx Default Location.
