3edf738934beb467bbe24fc51a65752ecd7d5b5a
Lakshmi <narayan.training1@gmail.com>
2023-06-21 19:10:15 +0530

    autopush

--- roboshop/Components/frontend.sh
+++ roboshop/Components/frontend.sh
@@ -8,33 +8,27 @@ if [ $ID -ne 0 ] ; then
     exit 1
 fi
 
-echo -n "Installing nginx :"
-yum install nginx -y &>> "/tmp/${COMPONENT}.log"
+stat() {
 
-if [ $? -eq 0 ] ; then
+    if [ $1 -eq 0 ] ; then
     echo -e "\e[32m Success \e[0m" 
 else
     echo -e "\e[31m Failure \e[0m" 
 fi
+}
+
+echo -n "Installing nginx :"
+yum install nginx -y &>> "/tmp/${COMPONENT}.log"
+stat $?
 
 echo -n "Downloading the frontend component"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-
-if [ $? -eq 0 ] ; then
-    echo -e "\e[32m Success \e[0m" 
-else
-    echo -e "\e[31m Failure \e[0m" 
-fi
+stat $?
 
 echo -n "Performing cleanup"
 cd /usr/share/nginx/html
 rm -rf * &>> "/tmp/${COMPONENT}.log"
-
-if [ $? -eq 0 ] ; then
-    echo -e "\e[32m Success \e[0m" 
-else
-    echo -e "\e[31m Failure \e[0m" 
-fi
+stat $?
 
 
 # cd /usr/share/nginx/html
