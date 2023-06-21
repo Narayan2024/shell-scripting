c06512ed7beede63e5afc02213129c594d9f7707
Lakshmi <narayan.training1@gmail.com>
2023-06-21 18:36:08 +0530

    autopush

--- roboshop/Components/frontend.sh
+++ roboshop/Components/frontend.sh
@@ -26,6 +26,24 @@ else
     echo -e "\e[32m Failure \e[0m" 
 fi
 
+echo -n "Performing cleanup"
+cd /usr/share/nginx/html
+rm -rf * &>> "/tmp/${COMPONENT}.log"
+
+if [ $? -eq 0 ] ; then
+    echo -e "\e[32m Success \e[0m" 
+else
+    echo -e "\e[32m Failure \e[0m" 
+fi
+
+
+# cd /usr/share/nginx/html
+# rm -rf *
+# unzip /tmp/frontend.zip
+# mv frontend-main/* .
+# mv static/* .
+# rm -rf frontend-main README.md
+# mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
 # ```
 # # curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
@@ -75,3 +93,12 @@ fi
 # ```
 
 # Deploy in Nginx Default Location.
+
+
+# cd /usr/share/nginx/html
+# rm -rf *
+# unzip /tmp/frontend.zip
+# mv frontend-main/* .
+# mv static/* .
+# rm -rf frontend-main README.md
+# mv localhost.conf /etc/nginx/default.d/roboshop.conf
