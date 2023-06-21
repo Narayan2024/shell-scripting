c41f54a66210e3c8a1f9b965080fe401cf022337
Lakshmi <narayan.training1@gmail.com>
2023-06-21 22:51:36 +0530

    autopush

--- roboshop/Components/mongodb.sh
+++ roboshop/Components/mongodb.sh
@@ -41,9 +41,21 @@ systemctl enable mongod &>> $LOGFILE
 systemctl start mongod &>> $LOGFILE
 stat $?
 
-echo -n "Downloading the schema :"
+echo -n "Downloading the $COMPONENT schema :"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
 stat $?
+
+echo -n "Extracting the $COMPONENT schema :"
+cd /tmp
+unzip mongodb.zip &>> $LOGFILE
+stat $?
+
+echo -n "Injecting the schema :"
+cd mongodb-main
+mongo < catalogue.js
+mongo < users.js
+stat $?
+
 
 
 
