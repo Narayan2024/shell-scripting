de14cc0b63bc5c67c1a0977adef96dd7603492a1
Lakshmi <narayan.training1@gmail.com>
2023-06-21 15:05:47 +0530

    autopush

--- roboshop/Components/frontend.sh
+++ roboshop/Components/frontend.sh
@@ -2,7 +2,7 @@
 
 ID=$(id -u)
 if [ $ID -ne 0 ] ; then
-    echo -m "\e[31m This script is expected to be used by a root user or with a sudo privilege \e[0m]"
+    echo -e "\e[31m This script is expected to be used by a root user or with a sudo privilege \e[0m]"
     exit 1
 fi
 
