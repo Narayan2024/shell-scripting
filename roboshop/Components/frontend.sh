e6b386d02e517908cb25c755b03109f8cb333f8d
Lakshmi <narayan.training1@gmail.com>
2023-06-21 16:32:38 +0530

    autopush

--- roboshop/Components/frontend.sh
+++ roboshop/Components/frontend.sh
@@ -17,20 +17,14 @@ else
     echo -e "\e[32m Failure \e[0m" 
 fi
 
+echo -n "Downloading the frontend component"
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
-
-# The frontend is the service in RobotShop to serve the web content over Nginx.
-
-# Install Nginx.
-
-# ```
-# # yum install nginx -y
-# # systemctl enable nginx
-# # systemctl start nginx
-
-# ```
-
-# Let's download the HTDOCS content and deploy it under the Nginx path.
+if [ $? -eq 0 ] ; then
+    echo -e "\e[32m Success \e[0m" 
+else
+    echo -e "\e[32m Failure \e[0m" 
+fi
 
 # ```
 # # curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
