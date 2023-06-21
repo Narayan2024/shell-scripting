34403aede858a20c1d24e8580ae6db522bf733ab
Lakshmi <narayan.training1@gmail.com>
2023-06-21 19:44:24 +0530

    autopush

--- roboshop/Components/frontend.sh
+++ roboshop/Components/frontend.sh
@@ -40,7 +40,7 @@ mv static/* . &>> $LOGFILE
 rm -rf ${COMPONENT}-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
-echo -n "Starting ${COMPONENT} service"
+echo -n "Starting ${COMPONENT} service :"
 systemctl enable nginx
 systemctl start nginx
 stat $?
