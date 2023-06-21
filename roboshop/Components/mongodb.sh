5468b57176132533a8a4c8f7b046304e6d974b41
Lakshmi <narayan.training1@gmail.com>
2023-06-21 20:24:27 +0530

    autopush

--- roboshop/Components/mongodb.sh
+++ roboshop/Components/mongodb.sh
@@ -23,7 +23,7 @@ echo -n "Configuring the repo :"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
 stat $?
 
-echo -n "Installing ${ COMPONENT }"
+echo -n "Installing ${COMPONENT} :"
 yum install -y mongodb-org &>> $LOGFILE
 stat$?
 
