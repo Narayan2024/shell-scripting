cf6d9b4c155085efebbca21f0f271c5c7ae434d4
Lakshmi <narayan.training1@gmail.com>
2023-06-22 10:31:21 +0530

    autopush

--- roboshop/Components/catalogue.sh
+++ roboshop/Components/catalogue.sh
@@ -1,3 +1,24 @@
 #! /bin/bash
 
-echo "I am catalogue"
+COMPONENT=catalogue
+LOGFILE="/tmp/${COMPONENT}.log"
+
+ID=$(id -u)
+if [ $ID -ne 0 ] ; then
+    echo -e "\e[31m This script is expected to be used by a root user or with a sudo privilege \e[0m]"
+    exit 1
+fi
+
+stat() {
+
+    if [ $1 -eq 0 ] ; then
+    echo -e "\e[32m Success \e[0m" 
+else
+    echo -e "\e[31m Failure \e[0m" 
+    exit 2
+fi
+}
+echo -n "Installing Node.js :"
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
+yum install nodejs -y
+stat $?
