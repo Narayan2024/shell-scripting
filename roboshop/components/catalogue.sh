00df77c10261864cc8b207285f1fea00ddab4425
Lakshmi <narayan.training1@gmail.com>
2023-06-24 14:11:03 +0530

    autopush

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -1,24 +1,8 @@
 #! /bin/bash
 
 COMPONENT=catalogue
-LOGFILE="/tmp/${COMPONENT}.log"
-APPUSER="roboshop"
 
-ID=$(id -u)
-if [ $ID -ne 0 ] ; then
-    echo -e "\e[31m This script is expected to be used by a root user or with a sudo privilege \e[0m]"
-    exit 1
-fi
-
-stat() {
-
-    if [ $1 -eq 0 ] ; then
-    echo -e "\e[32m Success \e[0m" 
-else
-    echo -e "\e[31m Failure \e[0m" 
-    exit 2
-fi
-}
+source components/common.sh
 
 echo -e "************\e[35m Installation has started \e[0m ************"
 
--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 
 COMPONENT=frontend
-LOGFILE="/tmp/${COMPONENT}.log"
 
-ID=$(id -u)
-if [ $ID -ne 0 ] ; then
-    echo -e "\e[31m This script is expected to be used by a root user or with a sudo privilege \e[0m]"
-    exit 1
-fi
-
-stat() {
-
-    if [ $1 -eq 0 ] ; then
-    echo -e "\e[32m Success \e[0m" 
-else
-    echo -e "\e[31m Failure \e[0m" 
-    exit 2
-fi
-}
+source components/common.sh
 
 echo -n "Installing nginx :"
 yum install nginx -y &>> $LOGFILE
