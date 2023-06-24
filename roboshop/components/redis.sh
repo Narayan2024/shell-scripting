95a6c5659033df70580b7bd5b6f5e064d3b0983b
Lakshmi <narayan.training1@gmail.com>
2023-06-24 14:37:18 +0530

    autopush

---
+++ roboshop/components/redis.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+COMPONENT=redis
+
+source components/common.sh 
+
+echo -e "************\e[35m Installation has started \e[0m ************"
+
+echo -n "Configuring the repo :"
+curl -L https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/${COMPONENT}.repo -o /etc/yum.repos.d/${COMPONENT}.repo
+stat $?
+
+echo -n "Installing ${COMPONENT} :"
+yum install -y ${COMPONENT}-6.2.11  &>> $LOGFILE
+stat $?
+
+echo -n "Enabling the DB visibility"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/${COMPONENT}.conf
+stat $?
+
+echo -n "Starting ${COMPONENT} :"
+systemctl enable ${COMPONENT} &>> $LOGFILE
+systemctl start ${COMPONENT} &>> $LOGFILE
+stat $?
+
+echo -e "************\e[35m Installation has completed \e[0m ************"
+
+
+
+
