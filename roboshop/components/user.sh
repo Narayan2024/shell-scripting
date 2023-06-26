36f8668c8e39bda0cd5a9443a2b5f5a837dca526
Lakshmi <narayan.training1@gmail.com>
2023-06-26 17:05:45 +0530

    autopush

--- roboshop/components/user.sh
+++ roboshop/components/user.sh
@@ -19,13 +19,13 @@ stat $?
 fi
 
 echo -n "Downloading the $COMPONENT component :"
-curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip"
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
 stat $?
 
 echo -n "Copying the $COMPONENT to $APPUSER home directory :"
 cd /home/${APPUSER}
 rm -rf ${COMPONENT} &>> $LOGFILE
-unzip -o /tmp/${COMPONENT}.zip &>> $LOGFILE
+unzip -o /tmp/${COMPONENT}.zip  &>> $LOGFILE
 stat $?
 
 echo -n "Modifying the Ownership :"
