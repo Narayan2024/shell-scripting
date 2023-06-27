b8adeda8b4025890d5aab2929a280b9c05f8abdc
Lakshmi <narayan.training1@gmail.com>
2023-06-27 19:11:04 +0530

    autopush

--- roboshop/components/user.sh
+++ roboshop/components/user.sh
@@ -6,27 +6,27 @@ source components/common.sh
 
 echo -e "************\e[35m Installation has started \e[0m ************"
 
-# echo -n "Configuring $COMPONENT repo and Installing Nodejs  :"
-# curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>> $LOGFILE
-# yum install nodejs -y &>> $LOGFILE
-# stat $?
+echo -n "Configuring $COMPONENT repo and Installing Nodejs  :"
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>> $LOGFILE
+yum install nodejs -y &>> $LOGFILE
+stat $?
 
-# id $APPUSER &>> $LOGFILE
-# if [ $? -ne 0 ] ; then
-# echo -n "Creating the service account :"
-# useradd $APPUSER &>> $LOGFILE
-# stat $?
-# fi
+id $APPUSER &>> $LOGFILE
+if [ $? -ne 0 ] ; then
+echo -n "Creating the service account :"
+useradd $APPUSER &>> $LOGFILE
+stat $?
+fi
 
-# echo -n "Downloading the $COMPONENT component :"
-# curl -s -L -o /tmp/user.zip "https://github.com/stans-robot-project/user/archive/main.zip"
-# stat $?
+echo -n "Downloading the $COMPONENT component :"
+curl -s -L -o /tmp/user.zip "https://github.com/stans-robot-project/user/archive/main.zip"
+stat $?
 
-# echo -n "Copying the $COMPONENT to $APPUSER home directory :"
-# cd /home/${APPUSER}/
-# rm -rf ${COMPONENT} &>> $LOGFILE
-# unzip -o /tmp/${COMPONENT}.zip  &>> $LOGFILE
-# stat $?
+echo -n "Copying the $COMPONENT to $APPUSER home directory :"
+cd /home/${APPUSER}/
+rm -rf ${COMPONENT} &>> $LOGFILE
+unzip -o /tmp/${COMPONENT}.zip  &>> $LOGFILE
+stat $?
 
 echo -n "Modifying the Ownership :"
 mv $user-main/$COMPONENT
@@ -43,11 +43,11 @@ sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_DNSNAME/mongod
 mv /home/${APPUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>> $LOGFILE
 stat $?
 
-# echo -n "Starting user service : "
-# systemctl daemon-reload &>> $LOGFILE
-# systemctl enable user &>> $LOGFILE
-# systemctl restart user &>> $LOGFILE
-# stat $?
+echo -n "Starting user service : "
+systemctl daemon-reload &>> $LOGFILE
+systemctl enable user &>> $LOGFILE
+systemctl restart user &>> $LOGFILE
+stat $?
 
 
 echo -e "************\e[35m Installation has completed \e[0m ************"
