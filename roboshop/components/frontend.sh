2bcb4a821b773a29c9f1be4fc97cb20ce31fd0c8
Lakshmi <narayan.training1@gmail.com>
2023-06-28 13:51:12 +0530

    autopush

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -26,14 +26,8 @@ mv static/* . &>> $LOGFILE
 rm -rf ${COMPONENT}-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 stat $?
-
-echo -n "Updating the backend component reverseproxy details :"
-for component in catalogue ; do
-sed -i -e "/$component/s/localhost/$component.roboshop.internal/" /etc/nginx/default.d/roboshop.conf
-done
-stat $?
-
-echo -n "Updating the backend component revers proxy details : "
+ 
+echo -n "Updating the backend component reverse proxy details : "
 for component in catalogue user ; do
     sed -i -e "/$component/s/localhost/$component.roboshop.internal/" /etc/nginx/default.d/roboshop.conf
 done
