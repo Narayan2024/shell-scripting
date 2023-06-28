234d6fff4d1bad8447b2b86a709ff266990e3d71
Lakshmi <narayan.training1@gmail.com>
2023-06-28 22:15:21 +0530

    autopush

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -4,66 +4,6 @@ COMPONENT="catalogue"
 
 source components/common.sh
 
-echo -e "************\e[35m Installation has started \e[0m ************"
-
-echo -n "Configuring $COMPONENT repo and Installing Nodejs  :"
-curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>> $LOGFILE
-yum install nodejs -y &>> $LOGFILE
-stat $?
-
-id $APPUSER &>> $LOGFILE
-if [ $? -ne 0 ] ; then
-echo -n "Creating the service account :"
-useradd $APPUSER &>> $LOGFILE
-stat $?
-fi
-
-echo -n "Downloading the $COMPONENT component :"
-curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
-stat $?
-
-echo -n "Copying the $COMPONENT to $APPUSER home directory :"
-cd /home/${APPUSER}
-rm -rf ${COMPONENT} &>> $LOGFILE
-unzip -o /tmp/${COMPONENT}.zip &>> $LOGFILE
-stat $?
-
-echo -n "Modifying the Ownership :"
-mv $COMPONENT-main/ $COMPONENT
-chown -R $APPUSER:$APPUSER /home/roboshop/$COMPONENT/ 
-stat $?
-
-echo -n "Generating npm $COMPONENT artifacts :"
-cd /home/${APPUSER}/${COMPONENT}/
-npm install &>> $LOGFILE
-stat $?
-
-echo -n "Updating the $COMPONENT systemd file :"
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/${APPUSER}/$COMPONENT systemd.service &>> $LOGFILE
-mv /home/${APPUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>> $LOGFILE
-stat $?
-
-echo -n "Starting ${COMPONENT} service : "
-systemctl daemon-reload &>> $LOGFILE
-systemctl enable catalogue &>> $LOGFILE
-systemctl restart catalogue &>> $LOGFILE
-stat $?
-
-
-echo -e "************\e[35m Installation has completed \e[0m ************"
-
-
-
-# **NOTE:** You should see the log saying `connected to MongoDB`, then only your catalogue
-# will work and can fetch the items from MongoDB
-
-# **Ref Log:**
-# {"level":"info","time":1656660782066,"pid":12217,"hostname":"ip-172-31-13-123.ec2.internal","msg":**"MongoDB connected",**"v":1}
-# ```
-
-# 1. Now, you would still see **`CATEGORIES`** on the frontend page as emp
-
-
-
+NODEJS              # calling nodejs function
 
 
--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -18,3 +18,73 @@ else
     exit 2
 fi
 }
+
+CREATE_USER (){
+    id $APPUSER &>> $LOGFILE
+if [ $? -ne 0 ] ; then
+echo -n "Creating the service account :"
+useradd $APPUSER &>> $LOGFILE
+stat $?
+fi
+}
+
+DOWNLOAD_AND_EXTRACT () {
+
+    echo -n "Downloading the $COMPONENT component :"
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+stat $?
+
+echo -n "Copying the $COMPONENT to $APPUSER home directory :"
+cd /home/${APPUSER}
+rm -rf ${COMPONENT} &>> $LOGFILE
+unzip -o /tmp/${COMPONENT}.zip &>> $LOGFILE
+stat $?
+
+echo -n "Modifying the Ownership :"
+mv $COMPONENT-main/ $COMPONENT
+chown -R $APPUSER:$APPUSER /home/roboshop/$COMPONENT/ 
+stat $?
+}
+
+NPM_INSTALL() {
+        echo -n "Generating npm $COMPONENT artifacts :"
+cd /home/${APPUSER}/${COMPONENT}/
+npm install &>> $LOGFILE
+stat $?
+
+}
+
+CONFIGURE_SVC () {
+
+    echo -n "Updating the $COMPONENT systemd file :"
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/${APPUSER}/$COMPONENT systemd.service &>> $LOGFILE
+mv /home/${APPUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>> $LOGFILE
+stat $?
+
+echo -n "Starting ${COMPONENT} service : "
+systemctl daemon-reload &>> $LOGFILE
+systemctl enable catalogue &>> $LOGFILE
+systemctl restart catalogue &>> $LOGFILE
+stat $?
+
+}
+
+NODEJS() {
+    echo -e "************\e[35m Installation has started \e[0m ************"
+
+echo -n "Configuring $COMPONENT repo and Installing Nodejs  :"
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>> $LOGFILE
+yum install nodejs -y &>> $LOGFILE
+stat $?
+
+echo -n "Installing Nodejs  :"
+yum install nodejs -y &>> $LOGFILE
+stat $?
+    
+CREATE_USER             # calling Create_user function to create the roboshop user account
+
+DOWNLOAD_AND_EXTRACT    # calling DOWNLOAD_AND_EXTRACT function to download the content
+
+NPM_INSTALL             # Creates artifacts
+
+}
