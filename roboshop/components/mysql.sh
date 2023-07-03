df8389fa657aa553aa0f7afb8b0f738b24b5dbbf
Lakshmi <narayan.training1@gmail.com>
2023-07-03 18:52:02 +0530

    autopush

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -23,7 +23,7 @@ stat $?
 
 # I want this to be executed only if the default password reset was not done
 echo "show databases;" | mysql -uroot -pRoboShop@1 &>> LOGFILE
-if [ $? -ne 0] ; then 
+if [ $? -ne 0 ] ; then 
 echo -n "Performing root user password reset : "
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql --connect-expired-password -uroot -p${DEFAULT_ROOT_PASSWORD}  &>> LOGFILE
 stat $? 
