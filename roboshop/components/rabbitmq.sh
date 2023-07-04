974e6319a316f753682c9143a7a77cd94bf3388b
Lakshmi <narayan.training1@gmail.com>
2023-07-04 14:02:01 +0530

    autopush

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -24,7 +24,7 @@ echo -n "Creating the $COMPONENT $APPUSER : "
 rabbitmqctl add_user roboshop roboshop123  &>> $LOGFILE
 stat $?
 
-echo -n "Configuring the $COMPONENT $APPUSER privileges : "  &>> $LOGFILE
+echo -n "Configuring the $COMPONENT $APPUSER privileges :"
 rabbitmqctl set_user_tags roboshop administrator   &>> $LOGFILE
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"    &>> $LOGFILE
 stat $?
