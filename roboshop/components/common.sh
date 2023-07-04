e3a53eb634128ca90a1f532cd82d44dbe893c4c4
Lakshmi <narayan.training1@gmail.com>
2023-07-04 16:19:26 +0530

    autopush

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -57,7 +57,7 @@ stat $?
 CONFIGURE_SVC () {
 
     echo -n "Updating the $COMPONENT systemd file :"
-sed -i -e 's/DBHOST/mysql.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/${APPUSER}/${COMPONENT}/systemd.service  &>> $LOGFILE
+sed -i  -e 's/AMQPHOST/rabbitmq.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/' -e 's/CARTHOST/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/${APPUSER}/${COMPONENT}/systemd.service  &>> $LOGFILE
 mv /home/${APPUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>> $LOGFILE
 stat $?
 
@@ -135,6 +135,8 @@ GROUPID=$(id -g roboshop)
 echo -n "Updating the uid and gid in the $COMPONENT.ini file :"
 sed -i -e "/^uid/ c uid=${USERID}" -e "/^gid/ c gid=${GROUPID}"  home/${APPUSER}/${COMPONENT}/${COMPONENT}.ini  &>> LOGFILE
 stat $?
+
+CONFIGURE_SVC
 
 
 echo -e "************\e[35m Installation has completed \e[0m ************"
