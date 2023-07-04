6e2518e2d2f79c8b1c66d0bdb548c6d6d86b6861
Lakshmi <narayan.training1@gmail.com>
2023-07-04 17:14:54 +0530

    autopush

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -58,7 +58,7 @@ CONFIGURE_SVC () {
 
 echo -n "Updating the $COMPONENT systemd file :"
 sed -i  -e 's/AMQPHOST/rabbitmq.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/' -e 's/CARTHOST/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/${APPUSER}/${COMPONENT}/systemd.service  &>> $LOGFILE
-mv /home/${APPUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service   &>> $LOGFILE
+mv /home/${APPUSER}/${COMPONENT}/systemd.servce /etc/systemd/system/${COMPONENT}.service   &>> $LOGFILE
 stat $?
 
 echo -n " Starting $COMPONENT service : "
