3eaac75c683e1a0aba5634a32da9a30c997fd5d7
Lakshmi <narayan.training1@gmail.com>
2023-07-07 12:36:14 +0530

    autopush

--- roboshop/components/cart.sh
+++ roboshop/components/cart.sh
@@ -10,7 +10,7 @@ NODEJS              # calling nodejs function
 # mv cart-main/ cart
 # chown -R roboshop:roboshop /home/roboshop/cart/
 # cd cart
-# npm install (npm was not installe properly)
+# npm install (npm was not installed properly and so node modules was not present in /home/roboshop/cart)
 
 # mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
 # sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/'  /etc/systemd/system/cart.service
