3610545f12ed040f01d7d088573893781873caf4
Lakshmi <narayan.training1@gmail.com>
2023-07-07 11:21:54 +0530

    autopush

--- roboshop/components/cart.sh
+++ roboshop/components/cart.sh
@@ -5,3 +5,11 @@ COMPONENT=cart
 source components/common.sh
 
 NODEJS              # calling nodejs function
+
+# unzip -o /tmp/cart.zip
+# mv cart-main/ cart
+# chown -R roboshop:roboshop /home/roboshop/cart/
+# cd cart
+# npm install
+
+# sed -i -e  's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/'  /etc/systemd/system/catalogue.service
