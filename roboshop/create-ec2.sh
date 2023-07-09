d9937f2cebf6737574511e3014aa2aefea38dce4
Lakshmi <narayan.training1@gmail.com>
2023-07-09 14:42:08 +0530

    autopush

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -9,5 +9,8 @@ echo -n "AMI ID used to launch EC2 $AMI_ID"
 echo -e " ****** Lauching the server ******* "
 aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro
 
+aws ec2 describe-security-groups --filters Name=group-name,Values=b54-allow-all
+
+
 
 
