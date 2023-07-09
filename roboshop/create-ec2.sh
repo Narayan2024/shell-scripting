8c39059caaeb78cb62581e0fce11a7e704516057
Lakshmi <narayan.training1@gmail.com>
2023-07-09 14:03:35 +0530

    autopush

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -4,5 +4,5 @@
 # aws ec2 describe-images --region us-east-1 --image-ids ami-1234567890EXAMPLE
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId') | sed -e 's/"//g'
-echo -n "AMI ID is $AMI_ID "
+echo -n "AMI ID is $AMI_ID"
 
