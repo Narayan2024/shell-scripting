8ee12c3af6307280c083aece379b0857768a5468
Lakshmi <narayan.training1@gmail.com>
2023-07-09 18:46:19 +0530

    autopush

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -1,14 +1,12 @@
 #!bin/bash
 
-# aws ec2 describe-images --region us-east-1 --image-ids ami-1234567890EXAMPLE
-
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId'|sed -e 's/"//g')
 SG_ID=(aws ec2 describe-security-groups --filters Name=group-name,Values=b54-allow-all | jq '.SecurityGroups[].GroupId'|sed -e 's/"//g')
 
 echo -n "AMI ID used to launch EC2 $AMI_ID"
 
 echo -e " ****** Lauching the server ******* "
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --tag-specifications 'ResourceType=instance,Tags=[{Key=name,Value=payment}]'|jq .
 
 
 
