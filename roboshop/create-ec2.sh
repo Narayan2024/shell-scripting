689fadf158592f8d3a960bd1a0b66ceabf07bc2f
Lakshmi <narayan.training1@gmail.com>
2023-07-09 23:24:08 +0530

    autopush

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -24,7 +24,7 @@ echo -e "\e[36m **** Creating DNS record for the $COMPONENT **** \e[0m"
 sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${IPADDRESS}/" route53.json > /tmp/record.json
 aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/record.json
 
-echo -e "\e[36m **** Creating DNS record for the $COMPONENT is completed **** \e[0m"
+echo -e "\e[36m **** Creating DNS record for the $COMPONENT has completed **** \e[0m"
 
 
 
