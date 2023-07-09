3c5bad722e176e187d91b26a1c0c920318480fae
Lakshmi <narayan.training1@gmail.com>
2023-07-09 19:14:15 +0530

    autopush

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -1,7 +1,5 @@
 #!bin/bash
 
-COMPONENT=$1
-
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId'|sed -e 's/"//g')
 SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=b54-allow-all | jq '.SecurityGroups[].GroupId'|sed -e 's/"//g')
 
