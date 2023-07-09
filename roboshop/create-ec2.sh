20006b6e7dea0ff710db3bf627b86478eb3f8aea
Lakshmi <narayan.training1@gmail.com>
2023-07-09 18:52:30 +0530

    autopush

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -8,7 +8,7 @@ SG_ID=(aws ec2 describe-security-groups --filters Name=group-name,Values=b54-all
 echo -n "AMI ID used to launch EC2 $AMI_ID"
 
 echo -e " ****** Lauching the server ******* "
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --tag-specifications 'ResourceType=instance,Tags=[{Key=name,Value=$COMPONENT}]' | jq .
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=$COMPONENT}]" | jq .
 
 
 
